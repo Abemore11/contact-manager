d7585c297d8b52c98cd07a39a7ebd7eded610345
abemore11 <abr.moreno@outlook.com>
2026-06-27 17:35:31 -0600

    Team 4th

---
+++ scripts/ui/navigation.js
@@ -0,0 +1,18 @@
+export function enableNavigation() {
+
+    const menuButton = document.querySelector("#mobile-drawer-toggle");
+
+    menuButton.addEventListener("click", () => {
+
+        const drawer = document.querySelector("#mobile-drawer");
+
+        drawer.classList.toggle("show");
+
+        if (drawer.classList.contains("show")) {
+            menuButton.setAttribute("aria-expanded", "true");
+        }
+        else {
+            menuButton.setAttribute("aria-expanded", "false");
+        }
+    });
+}
